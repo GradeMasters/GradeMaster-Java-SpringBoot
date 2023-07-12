5c488a5cedfc9785a1b5b833e6d257ffe7e10060
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-07-12 16:23:41 -0500

    saving

---
+++ src/main/java/com/grademaster/grademasterapp/GradeMasterAppApplication.java
@@ -0,0 +1,13 @@
+package com.grademaster.grademasterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeMasterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeMasterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grademaster/grademasterapp/GradeMasterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grademaster.grademasterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeMasterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
